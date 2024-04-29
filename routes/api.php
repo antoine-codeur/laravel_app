fcd176f5b16f5cddc14be7e007783ee40f12847e
antoine-codeur <antoinejf.denoue@gmail.com>
2024-04-29 14:02:07 +0200

    creation des routes et mise à jour de welcome.blade.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,85 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Endpoints API:
+// Accès publique page de connexion v1 :
+Route::get('/v1/welcome', function () {
+    return 'Récupération de texte à afficher sur la page de connexion/de bienvenue';
+});
+
+// Users v1 :
+Route::get('/v1/users', function () {
+    return 'Récupérer la liste des utilisateurs.';
+});
+
+Route::get('/v1/users/{id}', function ($id) {
+    return 'Récupérer un utilisateur spécifique par son identifiant: ' . $id;
+});
+
+Route::post('/v1/users', function () {
+    return 'Créer un nouvel utilisateur.';
+});
+
+Route::put('/v1/users/{id}', function ($id) {
+    return 'Mettre à jour les informations d\'un utilisateur existant: ' . $id;
+});
+
+Route::delete('/v1/users/{id}', function ($id) {
+    return 'Supprimer un utilisateur existant: ' . $id;
+});
+
+Route::post('/v1/login', function () {
+    return 'Endpoint pour l\'authentification.';
+});
+
+Route::post('/v1/register', function () {
+    return 'Endpoint pour l\'inscription d\'un nouvel utilisateur.';
+});
+
+// Products v1 :
+Route::get('/v1/products', function () {
+    return 'Récupérer la liste des produits.';
+});
+
+Route::get('/v1/products/{id}', function ($id) {
+    return 'Récupérer un produit spécifique par son identifiant: ' . $id;
+});
+
+Route::post('/v1/products', function () {
+    return 'Créer un nouveau produit.';
+});
+
+Route::put('/v1/products/{id}', function ($id) {
+    return 'Mettre à jour les informations d\'un produit existant: ' . $id;
+});
+
+Route::delete('/v1/products/{id}', function ($id) {
+    return 'Supprimer un produit existant: ' . $id;
+});
+
+// Catégories v1 :
+Route::get('/v1/categories', function () {
+    return 'Récupérer la liste des catégories.';
+});
+
+Route::get('/v1/categories/{id}', function ($id) {
+    return 'Récupérer une catégorie spécifique par son identifiant: ' . $id;
+});
+
+Route::post('/v1/categories', function () {
+    return 'Créer une nouvelle catégorie.';
+});
+
+Route::put('/v1/categories/{id}', function ($id) {
+    return 'Mettre à jour les informations d\'une catégorie existante: ' . $id;
+});
+
+Route::delete('/v1/categories/{id}', function ($id) {
+    return 'Supprimer une catégorie existante: ' . $id;
+});
+
+// Route test
+Route::get('/v1/test', function () {
+    return 'test';
+});
